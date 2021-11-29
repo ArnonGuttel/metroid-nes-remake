48fd75d1ddd103aacee500b3ca540fbecf6a6f77
Arnon Guttel <arnon.guttel@mail.huji.ac.il>
2021-11-29 13:03:40 +0200

    Enemy2

---
+++ Assets/Mechanics/5 Enemy2/EnemyBulletScript.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyBulletScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            print("You got hit!");
+            Destroy(gameObject);
+        }
+    }
+}
