ccdc321fc036dfba07c02c96c31d889cd4ff6203
Arnon Guttel <arnon.guttel@mail.huji.ac.il>
2021-12-18 00:11:26 +0200

    Game Update

--- Assets/Prototype/Scripts/Camera/BoundaryManager.cs
+++ Assets/Prototype/Scripts/Camera/BoundaryManager.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 
 public class BoundaryManager : MonoBehaviour
 {
+
+    #region Inspector
+
     private BoxCollider2D managerBox;
     public Transform Player;
     public GameObject boundary;
+
+    #endregion
+    
+    #region Fields
+    
+    // Those variables will be allow the Designer to limit the camera movement in X-axis or Y-axis 
 
     public bool XMaxEnabled;
     public float XMaxValue;
@@ -18,18 +27,29 @@ public class BoundaryManager : MonoBehaviour
 
     public bool YMinEnabled;
     public float YMinValue;
-    
+
+    #endregion
+
+    #region MonoBehaviour
+
     private void Start()
     {
         managerBox = GetComponent<BoxCollider2D>();
     }
-
     private void Update()
     {
         ManageBoundary();
     }
+    
+    #endregion
 
+    #region Methods
+    
+    
     private void ManageBoundary()
+        // This method will ensure that at any given time there will be only one active Boundary.
+        // The active Boundary will depend on the Player position.
+        // We will use the Boundary on the Camera Follows Script.
     {
         if (managerBox.bounds.min.x < Player.position.x && Player.position.x < managerBox.bounds.max.x &&
             managerBox.bounds.min.y < Player.position.y && Player.position.y < managerBox.bounds.max.y)
@@ -41,4 +61,7 @@ public class BoundaryManager : MonoBehaviour
             boundary.SetActive(false);
         }
     }
+
+    #endregion
+
 }
--- Assets/Prototype/Scripts/Camera/CameraFollows.cs
+++ Assets/Prototype/Scripts/Camera/CameraFollows.cs
@@ -1,40 +1,40 @@
-
 using UnityEngine;
 
 public class CameraFollows : MonoBehaviour
 {
+    #region Inspector
 
-    public Transform Player;
     [SerializeField] private float cameraSpeed;
+    [SerializeField] private Transform Player;
+    
+    #endregion
+
+    #region Fields
+
     private Vector3 _target;
     private GameObject _prevBoundary;
+
+    #endregion
+
+    #region MonoBehaviour
 
     void Update()
     {
         GameObject boundary = GameObject.Find("Boundary");
         followPlayer(boundary);
         if (boundary != _prevBoundary)
-        {   
-            if (_prevBoundary)
-                GameManager.boundrayChange();
-            if (_target != transform.position)
-            {
-                Player.GetComponent<PlayerMovement>().LockMovement = true;
-                Player.GetComponent<PlayerFireScript>().canFire = false;
-            }
-            else
-            {
-                Player.GetComponent<PlayerMovement>().LockMovement = false;
-                Player.GetComponent<PlayerFireScript>().canFire = true;
-                _prevBoundary = boundary;
-            }
-        }
+            CameraBoundaryChange(boundary);
     }
 
     private void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position,_target,cameraSpeed*Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _target, cameraSpeed * Time.deltaTime);
     }
+
+    #endregion
+
+
+    #region Methods
 
     private void followPlayer(GameObject boundary)
     {
@@ -42,24 +42,41 @@ public class CameraFollows : MonoBehaviour
         {
             Vector3 playerPosition = Player.transform.position;
             BoundaryManager script = boundary.GetComponentInParent<BoundaryManager>();
+            
             if (script.XMaxEnabled && script.XMinEnabled)
                 _target.x = Mathf.Clamp(playerPosition.x, script.XMinValue, script.XMaxValue);
             else if (script.XMaxEnabled)
                 _target.x = Mathf.Clamp(playerPosition.x, playerPosition.x, script.XMaxValue);
             else if (script.XMinEnabled)
                 _target.x = Mathf.Clamp(playerPosition.x, script.XMinValue, playerPosition.x);
-            
+
             if (script.YMaxEnabled && script.YMinEnabled)
                 _target.y = Mathf.Clamp(playerPosition.y, script.YMinValue, script.YMaxValue);
             else if (script.YMaxEnabled)
                 _target.y = Mathf.Clamp(playerPosition.y, playerPosition.y, script.XMaxValue);
             else if (script.YMinEnabled)
                 _target.y = Mathf.Clamp(playerPosition.y, script.XMinValue, playerPosition.y);
-            
-            _target.z = -10;
 
-            
+            _target.z = -10;
         }
     }
-    
+
+    private void CameraBoundaryChange(GameObject boundary)
+    {
+        if (_prevBoundary)
+            GameManager.boundrayChange();
+        if (_target != transform.position)
+        {
+            Player.GetComponent<PlayerMovement>().LockMovement = true;
+            Player.GetComponent<PlayerFireScript>().canFire = false;
+        }
+        else
+        {
+            Player.GetComponent<PlayerMovement>().LockMovement = false;
+            Player.GetComponent<PlayerFireScript>().canFire = true;
+            _prevBoundary = boundary;
+        }
+    }
+
+    #endregion
 }
